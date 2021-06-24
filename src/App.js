8e330adc3ed257e29a4781bb0d19f4f3bb91ccc0
Hafiz Ali <mohsantahir@yahoo.com>
2021-06-23 22:17:36 -0400

    Added Changes regarding navbar and footbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 //DEPENDENCIES
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom';
+import { apiURL } from "./util/apiURL";
+import axios from "axios";
+import "./App.css";
 
+
+//COMPONENTS
+import NavBar from './Components/NavBar';
+import FootBar from './Components/FootBar';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        Hello
+      <div className="App">
+        <NavBar />
+        <main>
+
+        </main>
+        <FootBar />
       </div>
     )
   }
