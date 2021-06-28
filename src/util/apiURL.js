e3a6f522b31d77aec06c7edd86de6803dbfd9eb3
Hafiz Ali <mohsantahir@yahoo.com>
2021-06-28 00:03:09 -0400

    Removed / from end of url

--- src/util/apiURL.js
+++ src/util/apiURL.js
@@ -1,5 +1,5 @@
 export const apiURL = () => {
     return window.location.hostname === "localhost"
     ? "http://127.0.0.1:3003"
-    : " https://fullstack-budget.herokuapp.com/";
+    : " https://fullstack-budget.herokuapp.com";
 };
